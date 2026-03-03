2b36c0394b400f0e646546207dffd1a568744cec
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-03 14:07:26 +0530

    Updated project files

---
+++ practice/pp19.py
@@ -0,0 +1,17 @@
+class Demo:
+    a=10 #<----static variable
+    def __init__(self):
+        self.b=20
+        self.c=21
+    def instanceDisp(self):  #<----instance method
+        print(f"the value of b :{self.b} and c :{self.c} ")
+    @staticmethod
+    def staticDisp():        #<----static method
+        print(f"the value of a is :{Demo.a}")
+    @classmethod
+    def classDisp(cls):      #<----class  method
+        print("the value os a b c is above") 
+d=Demo()
+d.instanceDisp()
+Demo.staticDisp()
+Demo.classDisp()  
