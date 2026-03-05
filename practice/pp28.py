658bd6f405f3fd8276fa3602e73d85d92846cb1c
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-05 20:41:49 +0530

    Updated project files

---
+++ practice/pp28.py
@@ -0,0 +1,23 @@
+def pal(n):
+    temp=n
+    if n<0:
+        n*=-1
+    rev=0
+    while n>0:
+        rem=n%10
+        rev=(rev*10)+rem
+        n//=10
+    if temp<0:
+        rev*=-1
+    return temp==rev
+s1=int(input("starting"))
+s2=int(input("starting"))
+if s1>s2:
+    print("s1 shd be smaller")
+else:
+    print("palidrom")
+    for i in range(s1,s2+1):
+        flag=pal(i)
+        if flag:
+            print(i,end=" ")
+            
