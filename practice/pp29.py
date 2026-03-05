fa72c1d67d8859ba1d8900e2c5cf814231cb580d
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-05 21:11:42 +0530

    Updated project files

--- practice/pp29.py
+++ practice/pp29.py
@@ -1,6 +1,6 @@
 def eo(n):
     return n%2==0
-num=int(input("enter the number : "))
+n=int(input("enter the number : "))
 
 # while num>0:
 #     flag=eo(num)
@@ -8,13 +8,21 @@ num=int(input("enter the number : "))
 #         print(num,end=" ")
 #     num-=1
 
-print("even")
-for i in range(1,num+1):
-    flag=eo(i)
+# print("even")
+# for i in range(1,num+1):
+#     flag=eo(i)
+#     if flag:
+#         print(i,end=" ")
+# print("\nodd")
+# for i in range(1,num+1):
+#     flag=eo(i)
+#     if not flag:
+#         print(i,end=" ")
+count=n
+num=1
+while count>0:
+    flag=eo(num)
     if flag:
-        print(i,end=" ")
-print("\nodd")
-for i in range(1,num+1):
-    flag=eo(i)
-    if not flag:
-        print(i,end=" ")
+        print(num,end=" ")
+        count-=1
+    num+=1
