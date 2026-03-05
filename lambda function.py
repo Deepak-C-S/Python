fd5771de3c2742bc5ba40335ab15f8bf42cd2cde
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-05 20:42:54 +0530

    Updated project files

--- lambda function.py
+++ lambda function.py
@@ -1,4 +1,4 @@
 s=lambda a,b:(a**2)+2*a*b+(b**2)
-a=int(input("Enter the value of a: "))
-b=int(input("Enter the value of b: "))
+a=int(input("Enter the value of a "))
+b=int(input("Enter the value of b "))
 print(s(a,b))
