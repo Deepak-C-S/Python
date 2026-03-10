9a558fc9052cecdc16a3c3bcf95f2a7db38642f2
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-10 14:13:41 +0530

    Updated project files

---
+++ practice/pp35.py
@@ -0,0 +1,23 @@
+a=10
+def fun1():
+    print(a)
+    b=20
+    print(b)
+fun1()
+
+def outer():
+    global a
+    a=100
+    c=30
+    print("c",c)
+    def inner():
+        print(a)
+        c=300
+        print("after c",c)
+        d=40
+    inner()
+    print("c outside inner",c) 
+outer()
+
+
+
---
+++ practice/pp39.py
@@ -0,0 +1,20 @@
+def fun(a):
+    print("inside fun1")
+def fun(a,b):
+    print("inside fun2")
+def fun(a,b,c):
+    print("inside fun3")
+def fun(a,b,c,d):
+    print("inside fun4")
+# fun()
+#another method
+def fun():
+    print("inside fun1")
+def fun():
+    print("inside fun2")
+def fun():
+    print("inside fun3")
+def fun():
+    print("inside fun4")
+fun()
+# the function doesn't support function over loading
