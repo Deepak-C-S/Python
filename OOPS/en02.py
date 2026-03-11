050691209d449d14e135b32bef8f609c795e0a46
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-11 14:39:58 +0530

    Updated project files

---
+++ OOPS/en02.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, name="", age="", usn=""):
+        self.name = name
+        self.age = age
+        self.__usn = usn
+
+    def get_usn(self):
+        return self.__usn
+
+    def set_usn(self, usn):
+        self.__usn = usn
+
+    usn = property(get_usn, set_usn)
+
+    def display(self):
+        print(f"The name is {self.name}")
+        print(f"The age is {self.age}")
+        print(f"The usn is {self.__usn}")
+
+
+d = Person("dcs", 21)
+d.usn = "012"      # setter
+print(d.usn)       # getter
+d.display()
