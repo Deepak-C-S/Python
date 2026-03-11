226ffbac8a3f85dc237b801749fb822c2282c6c7
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-11 14:17:26 +0530

    Updated project files

---
+++ OOPS/en01.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self,value):
+        self.__pages=value
+    def setter(self,value):
+        if value>=1:
+            self.__pages=value
+    def getter(self):
+        return self.__pages
+b=Book(00)
+b.setter(1154)
+res=b.getter()
+print(res)
+b.setter(15)
+res=b.getter()
+print(res)
+
