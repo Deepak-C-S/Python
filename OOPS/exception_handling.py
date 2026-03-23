e8718fbea478e8fed4bb68f71b073da7acc9df50
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-23 14:43:54 +0530

    Updated project files

--- OOPS/exception_handling.py
+++ OOPS/exception_handling.py
@@ -1,4 +1,7 @@
-n=int(input("num 1: "))
-m=int(input("num 2: "))
-c=n/m
-print(f"{n} by {m} is {c}")
+try:
+    n=int(input("num 1: "))
+    m=int(input("num 2: "))
+    print(f"{n} by {m} is {n/m}")
+
+except Exception as error:
+    print("error is ",error)
